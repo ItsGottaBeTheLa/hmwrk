6eda867af4ae716a60f05463fcf687b883feb1a4
Damiano Raab <DamianoRaab11@gmail.com>
2019-01-26 12:14:33 -0500

    sql info

---
+++ db/schema.sql
@@ -0,0 +1,21 @@
+DROP DATABASE IF EXISTS hmwrkdb;
+CREATE DATABASE hmwrkdb;
+
+use hmwrkdb;
+
+create table assignments
+(
+	id integer not null auto_increment,
+    assignment_name varchar(250) not null,
+    completed boolean default false,
+    assign_details DESCRIPTION not null VARCHAR(250),
+    primary key (id)
+    
+);
+
+create table users 
+(
+  id integer not null auto_increment,
+  username varchar(250) not null,
+  primary key (id) 
+);
