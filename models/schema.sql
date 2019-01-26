25cbbfad46906d31eef7925cff0dfd294bad286e
Damiano Raab <DamianoRaab11@gmail.com>
2019-01-26 11:57:12 -0500

    schema.sq/seeds.sql

--- models/schema.sql
+++ models/schema.sql
@@ -1,5 +1,20 @@
-DROP DATABASE IF EXISTS exampledb;
-CREATE DATABASE exampledb;
+DROP DATABASE IF EXISTS hmwrkdb;
+CREATE DATABASE hmwrkdb;
 
-DROP DATABASE IF EXISTS testdb;
-CREATE DATABASE testdb;
+use hmwrkdb;
+
+create table assignments
+(
+	id integer not null auto_increment,
+    assignment_name varchar(250) not null,
+    completed boolean default false,
+    assign_details DESCRIPTION not null VARCHAR(250),
+    primary key (id)
+    
+);
+
+create table users 
+(
+  username varchar(250) not null,
+  primary key (id) 
+);
