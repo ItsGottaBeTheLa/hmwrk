a665a17fdffe7827c03ad494df304ffd0398419f
bryant.gossett@gmail.com <5419632+Bgosse1@users.noreply.github.com>
2019-01-28 13:28:16 -0500

    WIP create new assignment

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -22,9 +22,9 @@ module.exports = function(app) {
     });
   });
   // Create a new example
-  app.post("/api/examples", function(req, res) {
-    db.Example.create(req.body).then(function(dbExample) {
-      res.json(dbExample);
+  app.post("/api/assignment", function(req, res) {
+    db.Example.create(req.body).then(function(dbAssignment) {
+      res.json(dbAssignment);
     });
   });
 
