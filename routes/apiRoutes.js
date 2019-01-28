7c89f84173da4f2b9e854b59fabef3f196ef86f2
bryant.gossett@gmail.com <5419632+Bgosse1@users.noreply.github.com>
2019-01-28 10:05:55 -0500

    added findall assignment endpoint

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -2,9 +2,9 @@ var db = require("../models");
 
 module.exports = function(app) {
   // Get all examples
-  app.get("/api/examples", function(req, res) {
-    db.Example.findAll({}).then(function(dbExamples) {
-      res.json(dbExamples);
+  app.get("/api/assignment", function(req, res) {
+    db.Assignment.findAll({}).then(function(dbAssignment) {
+      res.json(dbAssignment);
     });
   });
 
