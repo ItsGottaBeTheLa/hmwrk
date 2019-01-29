cbb29ca5c6dda74f2cb0fb5b8b2ffa45c463d1c4
bryant.gossett@gmail.com <5419632+Bgosse1@users.noreply.github.com>
2019-01-28 21:22:10 -0500

    merged master

--- routes/htmlRoutes.js
+++ routes/htmlRoutes.js
@@ -1,29 +1,38 @@
 var db = require("../models");
+var passport = require("passport");
 
 module.exports = function(app) {
   // Load index page
+
   app.get("/", function(req, res) {
-    db.Example.findAll({}).then(function(dbExamples) {
-      res.render("index", {
-        msg: "Welcome!",
-        examples: dbExamples
-      });
+    res.render("home", {
+      user: req.user
     });
   });
 
-  // Load example page and pass in an example by id
-  app.get("/example/:id", function(req, res) {
-    db.Example.findOne({ where: { id: req.params.id } }).then(function(
-      dbExample
-    ) {
-      res.render("example", {
-        example: dbExample
-      });
-    });
+  app.get("/login", function(req, res) {
+    res.render("login");
   });
 
-  // Render 404 page for any unmatched routes
-  app.get("*", function(req, res) {
-    res.render("404");
-  });
+  app.get("/login/github", passport.authenticate("github"));
+
+  app.get(
+    "/return",
+    passport.authenticate("github", {
+      failureRedirect: "/login"
+    }),
+    function(req, res) {
+      res.redirect("/");
+    }
+  );
+
+  app.get(
+    "/profile",
+    require("connect-ensure-login").ensureLoggedIn(),
+    function(req, res) {
+      res.render("profile", {
+        user: req.user
+      });
+    }
+  );
 };
