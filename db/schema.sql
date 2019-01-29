7fdc204e1a802366f4e8f52d227bf2fd6c3bfbb7
[verna100] <vernalizg@gmail.com>
2019-01-28 21:03:56 -0500

    passport integration

---
+++ db/schema.sql
@@ -0,0 +1,18 @@
+DROP DATABASE IF EXISTS hmwrkdb;
+CREATE DATABASE hmwrkdb;
+
+use hmwrkdb;
+
+CREATE TABLE assignments (
+    id INTEGER NOT NULL AUTO_INCREMENT,
+    assignmentName VARCHAR(250) NOT NULL,
+    completed BOOLEAN NOT NULL DEFAULT FALSE,
+    assignmentDetails TEXT NOT NULL,
+    PRIMARY KEY (id)
+);
+
+CREATE TABLE users (
+    id INTEGER NOT NULL AUTO_INCREMENT,
+    userName VARCHAR(250) NOT NULL,
+    PRIMARY KEY (id)
+);
