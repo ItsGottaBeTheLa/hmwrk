adccde0944f91401f102589690a6cb8a5c6f9c0a
bryant.gossett@gmail.com <5419632+Bgosse1@users.noreply.github.com>
2019-01-28 21:18:28 -0500

    clean master

--- models/assignment.js
+++ models/assignment.js
@@ -4,20 +4,15 @@ module.exports = function(sequelize, DataTypes) {
       type: DataTypes.STRING,
       allowNull: false
     },
-<<<<<<< HEAD
-=======
     type: {
       type: DataTypes.STRING,
       allowNULL: false
     },
->>>>>>> 1ae1358346dbf9d291c5e7eaa7ac39882f1d7082
     completed: {
       type: DataTypes.BOOLEAN,
       allowNull: false,
       defaultValue: false
     },
-<<<<<<< HEAD
-=======
     dueDate: {
       type: DataTypes.DATE,
       allowNull: false
@@ -26,7 +21,6 @@ module.exports = function(sequelize, DataTypes) {
       type: DataTypes.BOOLEAN,
       allowNull: false
     },
->>>>>>> 1ae1358346dbf9d291c5e7eaa7ac39882f1d7082
     assignmentDetails: {
       type: DataTypes.STRING,
       allowNULL: false
