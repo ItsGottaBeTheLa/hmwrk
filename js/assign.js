bed61313cc4e31997ca39b2f1fc5c64d9a0b493c
[verna100] <vernalizg@gmail.com>
2019-02-02 13:01:34 -0500

    add more handlebars

---
+++ js/assign.js
@@ -0,0 +1,65 @@
+// Make sure we wait to attach our handlers until the DOM is fully loaded.
+$(function() {
+  $(".delassignment").on("click", function(event) {
+    var id = $(this).data("id");
+
+    // Send the DELETE request.
+    $.ajax("/api/assignments/" + id, {
+      type: "DELETE"
+    }).then(function() {
+      console.log("deleted id ", id);
+      // Reload the page to get the updated list
+      location.reload();
+    });
+  });
+
+  $(".create-form").on("submit", function(event) {
+    // Make sure to preventDefault on a submit event.
+    event.preventDefault();
+
+    var newAssignment = {
+      assignmentName: $("#name").val(),
+      type: $("#type").val(),
+      completed: $("#completed").val(),
+      assignmentDetails: $("#details").val(),
+      dueDate: $("#due").val(),
+      isRequired: $("#required").val()
+    };
+
+    // Send the POST request.
+    $.ajax("/api/assignments", {
+      type: "POST",
+      data: newAssignment
+    }).then(function() {
+      console.log("created new assignment");
+      // Reload the page to get the updated list
+      location.reload();
+    });
+  });
+
+  $(".update-form").on("submit", function(event) {
+    // Make sure to preventDefault on a submit event.
+    event.preventDefault();
+
+    var updatedAssignment = {
+      assignmentName: $("#name").val(),
+      type: $("#type").val(),
+      completed: $("#completed").val(),
+      assignmentDetails: $("#details").val(),
+      dueDate: $("#due").val(),
+      isRequired: $("#required").val()
+    };
+
+    var id = $(this).data("id");
+
+    // Send the POST request.
+    $.ajax("/api/assignments/" + id, {
+      type: "PUT",
+      data: updatedAssignment
+    }).then(function() {
+      console.log("updated assignment");
+      // Reload the page to get the updated list
+      location.assign("/");
+    });
+  });
+});
