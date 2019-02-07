333212f7b0f420f9215725e6f0d29102b3e92142
bryant.gossett@gmail.com <5419632+Bgosse1@users.noreply.github.com>
2019-02-06 20:15:54 -0500

    fixed schema and seed errors

--- db/schema.sql
+++ db/schema.sql
@@ -6,7 +6,7 @@ CREATE DATABASE hmwrkdb;
 
 use hmwrkdb;
 
-CREATE TABLE assignments (
+CREATE TABLE Assignments (
     id INTEGER NOT NULL AUTO_INCREMENT,
     assignmentName VARCHAR(250) NOT NULL,
     type VARCHAR(25) NOT NULL,
@@ -20,7 +20,7 @@ CREATE TABLE assignments (
     PRIMARY KEY (id)
 );
 
-CREATE TABLE users (
+CREATE TABLE Users (
     id INTEGER NOT NULL AUTO_INCREMENT,
     userName VARCHAR(250) NOT NULL,
     isAdmin BOOLEAN NOT NULL,
